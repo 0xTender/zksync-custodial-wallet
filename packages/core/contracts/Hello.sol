5e6fbcf24efdcf2a1173f2458c0a824a994bc20e
Aniket Chowdhury <8.aniket.chowdhury@gmail.com>
2023-07-11 13:32:20 +0530

    feat: init for zksync

--- packages/core/contracts/Hello.sol
+++ packages/core/contracts/Hello.sol
@@ -1,30 +1,30 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.17;
 
 import "hardhat/console.sol";
 
 contract Hello {
-  event MessageEvent(string message);
+    event MessageEvent(string message);
 
-  struct EventStruct {
-    string message;
-    uint256 randomNumber;
-  }
+    struct EventStruct {
+        string message;
+        uint256 randomNumber;
+    }
 
-  event StructEvent(EventStruct eventStruct);
+    event StructEvent(EventStruct eventStruct);
 
-  constructor(string memory _message) {
-    console.log(_message);
-    message(_message);
-  }
+    constructor(string memory _message) {
+        console.log(_message);
+        message(_message);
+    }
 
-  function message(string memory _message) public {
-    // bad way to generate random number
-    // do not use in production
-    uint256 _randomNumber = block.timestamp * 100;
-    emit MessageEvent(_message);
+    function message(string memory _message) public {
+        // bad way to generate random number
+        // do not use in production
+        uint256 _randomNumber = block.timestamp * 100;
+        emit MessageEvent(_message);
 
-    emit StructEvent(EventStruct(_message, _randomNumber));
-  }
+        emit StructEvent(EventStruct(_message, _randomNumber));
+    }
 }
--- packages/core/hardhat.config.ts
+++ packages/core/hardhat.config.ts
@@ -4,37 +4,41 @@ import "hardhat-deploy-ethers";
 import "@typechain/hardhat";
 import "@nomicfoundation/hardhat-chai-matchers";
 
+import "@matterlabs/hardhat-zksync-deploy";
+import "@matterlabs/hardhat-zksync-solc";
+import "@matterlabs/hardhat-zksync-verify";
+// dynamically changes endpoints for local tests
+const zkSyncTestnet =
+  process.env.NODE_ENV == "test"
+    ? {
+        url: "http://localhost:3050",
+        ethNetwork: "http://localhost:8545",
+        zksync: true,
+      }
+    : {
+        url: "https://zksync2-testnet.zksync.dev",
+        ethNetwork: "goerli",
+        zksync: true,
+        // contract verification endpoint
+        verifyURL:
+          "https://zksync2-testnet-explorer.zksync.dev/contract_verification",
+      };
 const config: HardhatUserConfig = {
-  solidity: {
-    compilers: [
-      {
-        version: "0.8.18",
-      },
-    ],
+  zksolc: {
+    version: "latest",
+    settings: {
+      isSystem: true, // make sure to include this line
+    },
   },
+  defaultNetwork: "zkSyncTestnet",
   networks: {
     hardhat: {
-      blockGasLimit: 14_999_999,
-      chainId: 1337,
-      accounts: {
-        mnemonic:
-          process.env.MNEMONIC ??
-          "test test test test test test test test test test test junk",
-        count: 10,
-      },
+      zksync: false,
     },
-    ...(process.env.MNEMONIC
-      ? {
-          localhost: {
-            blockGasLimit: 14_999_999,
-            chainId: 1337,
-            accounts: {
-              mnemonic: process.env.MNEMONIC,
-              count: 10,
-            },
-          },
-        }
-      : {}),
+    zkSyncTestnet,
+  },
+  solidity: {
+    version: "0.8.17",
   },
   namedAccounts: {
     deployer: {
