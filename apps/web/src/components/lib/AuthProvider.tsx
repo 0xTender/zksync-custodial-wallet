a7ab9a689eb54936b8a233fcce88eb1ce6c7707c
Abhinav Robinson <abhinavrobinson@gmail.com>
2023-07-16 15:47:56 +0530

    fix: rm nextauth

--- apps/web/src/components/lib/AuthProvider.tsx
+++ apps/web/src/components/lib/AuthProvider.tsx
@@ -1,24 +1,17 @@
-"use client";
-
-import { SessionProvider } from "next-auth/react";
-
+import { useRouter } from "next/router";
+import { type PropsWithChildren, useEffect } from "react";
 import { useAccount } from "wagmi";
-import { useRouter } from "next/navigation";
-import { useEffect } from "react";
 
-// Use of the <SessionProvider> is mandatory to allow components that call
-// `useSession()` anywhere in your application to access the `session` object
-// We use client hydration to establish the <SessionProvider />
-export default function AuthProvider({
-  children,
-}: {
-  children?: React.ReactNode;
-}) {
-  const { isConnected } = useAccount();
+export default function AuthProvider({ children }: PropsWithChildren<unknown>) {
   const router = useRouter();
+  const { isConnected } = useAccount();
   useEffect(() => {
-    if (isConnected) router.push("/app");
-    else router.push("/");
+    if (isConnected) {
+      void router.push("/app");
+    } else {
+      void router.push("/");
+    }
   }, [isConnected, router]);
-  return <SessionProvider>{children}</SessionProvider>;
+
+  return <>{children}</>;
 }
--- apps/web/src/components/login/LoginButtons.tsx
+++ apps/web/src/components/login/LoginButtons.tsx
@@ -3,21 +3,6 @@ import MetamaskButton from "@app/components/buttons/MetamaskButton";
 export default function LoginButtons() {
   return (
     <>
-      <div>
-        <div className="relative my-10">
-          <div
-            className="absolute inset-0 flex items-center"
-            aria-hidden="true"
-          >
-            <div className="w-full border-t border-gray-200" />
-          </div>
-          <div className="relative flex justify-center text-sm font-medium leading-6">
-            <span className="bg-white px-6 text-gray-900">
-              Or continue with
-            </span>
-          </div>
-        </div>
-      </div>
       <div className="grid grid-cols-2 gap-4">
         <MetamaskButton />
         <div className="flex w-full select-none items-center justify-center gap-3 rounded-md bg-[#24292F] px-3 py-2 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[#24292F]">
