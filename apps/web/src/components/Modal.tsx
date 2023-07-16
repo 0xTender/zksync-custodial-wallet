b7b9d9d5c211e7c57a5d6f24a1baffed5b810d78
Abhinav Robinson <abhinavrobinson@gmail.com>
2023-07-16 18:59:54 +0530

    feat: grayscale

--- apps/web/src/components/Modal.tsx
+++ apps/web/src/components/Modal.tsx
@@ -1,16 +1,24 @@
 import { type PropsWithChildren } from "react";
+import { twMerge } from "tailwind-merge";
 
 export default function Modal({
   children,
   heading,
+  grayscale,
 }: PropsWithChildren<{
   heading: string | JSX.Element;
+  grayscale?: boolean;
 }>) {
   return (
     <div className="flex h-full flex-1 flex-col justify-center bg-zinc-900 py-12 sm:px-6 lg:px-8">
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-[480px]">
         <h1 className="my-4 text-xl text-zinc-100/90">{heading}</h1>
-        <div className="border-[0.5px] border-amber-800/50 bg-amber-800/5 p-6 sm:rounded-lg">
+        <div
+          className={twMerge(
+            "border-[0.5px] border-amber-800/50 bg-amber-800/5 p-6 sm:rounded-lg",
+            grayscale && "border-zinc-600 bg-zinc-800/25"
+          )}
+        >
           {children}
         </div>
       </div>
